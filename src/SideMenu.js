47afed7b15fa0ac312d6d7975084a38c0ba72b6b
C.Y.Cheung <c.y.cheung@CYCheung.local>
2022-04-06 23:17:44 +0800

    some text

--- src/SideMenu.js
+++ src/SideMenu.js
@@ -9,11 +9,11 @@ function SideMenu() {
             <div className="HeaderHeight"></div>
             <h6>This is a Side Menu</h6>
             <div className="container py-3" style={{'text-align': 'center'}}>
-                <a href='/homepage/#/'><button className="button-rainbow" style={{'width': '80%'}}>Page 1</button></a><br/>
-                <a href='/homepage/#/page2'><button className="button-rainbow" style={{'width': '80%'}}>Page 2</button></a><br/>
-                <a href='/homepage/#/page3'><button className="button-rainbow" style={{'width': '80%'}}>Page 3</button></a><br/>
-                <a href='/homepage/#/page4'><button className="button-rainbow" style={{'width': '80%'}}>Page 4</button></a><br/>
-                <a href='/homepage/#/page5'><button className="button-rainbow" style={{'width': '80%'}}>Page 5</button></a><br/>
+                <a href='/#/'><button className="button-rainbow" style={{'width': '80%'}}>Page 1</button></a><br/>
+                <a href='/#/page2'><button className="button-rainbow" style={{'width': '80%'}}>Page 2</button></a><br/>
+                <a href='/#/page3'><button className="button-rainbow" style={{'width': '80%'}}>Page 3</button></a><br/>
+                <a href='/#/page4'><button className="button-rainbow" style={{'width': '80%'}}>Page 4</button></a><br/>
+                <a href='/#/page5'><button className="button-rainbow" style={{'width': '80%'}}>Page 5</button></a><br/>
             </div>
             <h6>Time</h6>
             <Clock1 />
